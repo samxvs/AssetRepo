cd7d3a7ece4da90243a3c9fae8a083023797a68a
maam1 <49322962+maam1@users.noreply.github.com>
2019-04-17 15:58:19 -0700

    Add files via upload

---
+++ PushCustomerImageToK8.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+cd /opt/rancher
+./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+./rancher kubectl create -f /var/lib/jenkins/workspace/AcceleratePushCustToK8/PushCustomerImageToK8.yaml 
