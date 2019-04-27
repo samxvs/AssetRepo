8e3a81c6a4ee79d856b1842902300af4ba09b8bf
maam1 <49322962+maam1@users.noreply.github.com>
2019-04-27 11:39:12 -0700

    Update copyPackages.sh

--- copyPackages.sh
+++ copyPackages.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
 
+sudo su
 cp -r /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages
 cp -r /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Employee /opt/softwareag/IntegrationServer/packages
