b93290303b8ef53bfb580c649ef740acd9c809da
maam1 <49322962+maam1@users.noreply.github.com>
2019-04-27 11:55:23 -0700

    Delete CopyPackageToVM.sh

--- CopyPackageToVM.sh
+++
@@ -1,2 +0,0 @@
-cp -rf /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages
-cp -rf /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Employee /opt/softwareag/IntegrationServer/packages
