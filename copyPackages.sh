e2e35572e84b9e071dcd9c3fc2acbed8d8ca30df
maam1 <49322962+maam1@users.noreply.github.com>
2019-04-27 11:59:33 -0700

    Add files via upload

--- copyPackages.sh
+++ copyPackages.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-#
-
 sudo su
-cp -r /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages
-cp -r /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Employee /opt/softwareag/IntegrationServer/packages
+
+cp -rf /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages
