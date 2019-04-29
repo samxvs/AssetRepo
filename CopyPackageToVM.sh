69a8cb937240f50d288addb03bde0edb26fc849e
maam1 <49322962+maam1@users.noreply.github.com>
2019-04-29 02:10:50 -0700

    Update CopyPackageToVM.sh

--- CopyPackageToVM.sh
+++ CopyPackageToVM.sh
@@ -1,1 +1,1 @@
-cp -rf /var/lib/jenkins/workspace/Acc_MSR_CICD/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages
+cp -rf /var/lib/jenkins/workspace/AccelerateMSRCustomerImage/assets/IS/Packages/Customers /opt/softwareag/IntegrationServer/packages
