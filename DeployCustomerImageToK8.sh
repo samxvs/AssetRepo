6e6e687949d1db57c77b7018915522405338bd20
maam1 <49322962+maam1@users.noreply.github.com>
2019-04-29 12:50:59 -0700

    Update DeployCustomerImageToK8.sh

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -3,4 +3,4 @@
 
 cd /opt/rancher
 ./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
-./rancher kubectl create -f /var/lib/jenkins/workspace/AccelerateDeployCustToK8/PushCustomerImageToK8.yaml 
+./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
