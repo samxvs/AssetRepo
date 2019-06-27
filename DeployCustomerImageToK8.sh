a5fb71581be15adf5b08f6fc91dbeca316211974
samxvs <victor.sanchez@softwareag.com>
2019-06-27 08:27:37 -0500

    ajuste en token

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+./rancher login https://k8s.eur.ad.sag/v3 --token token-fnm89:ttf4bbbdmkc7txdc6xswrm92cbtfv6h45jvlxmhc8xzrx97vzjq5s8
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
