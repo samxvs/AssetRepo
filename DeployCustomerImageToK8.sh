2369be22cd3313fb2b624d155fae88969119dca6
samxvs <victor.sanchez@softwareag.com>
2021-10-13 21:43:35 -0500

    Update DeployCustomerImageToK8.sh
    ajuste en endpoint y token

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-fnm89:ttf4bbbdmkc7txdc6xswrm92cbtfv6h45jvlxmhc8xzrx97vzjq5s8
+./rancher login https://rancher.eur.ad.sag/v3 --token token-ls7hs:277qrq8kt82t88w87g5kdld98lvm7q2qcqf82xwvv8tmhhr9ncwstg
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
