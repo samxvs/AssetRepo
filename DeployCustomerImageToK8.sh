948e5e4f6fd577cc83fde607a89347461b77bd03
samxvs <victor.sanchez@softwareag.com>
2021-10-14 12:41:28 -0500

    Update DeployCustomerImageToK8.sh

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://rancher.eur.ad.sag/v3 --token token-ls7hs:277qrq8kt82t88w87g5kdld98lvm7q2qcqf82xwvv8tmhhr9ncwstg
+./rancher login https://10.248.75.144:8433/v3 --token token-ls7hs:277qrq8kt82t88w87g5kdld98lvm7q2qcqf82xwvv8tmhhr9ncwstg
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
